7d850a685c737c3cfb117057629825768fe0f51b
Debdut Chakraborty <debdutdeb@outlook.com>
2025-04-26 01:41:12 +0530

    use sudo while new install link only if needed

--- utils/new_install.go
+++ utils/new_install.go
@@ -5,17 +5,23 @@ does some new install stuff
 */
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"os"
 	"os/exec"
+	"os/user"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"syscall"
 	"time"
 
 	"github.com/debdutdeb/node-proxy/common"
 	"github.com/debdutdeb/node-proxy/state"
 )
+
+var errNotWriteable = fmt.Errorf("does not have permission to write to dir")
 
 func HandleNewInstall() error {
 	s, err := state.NewState()
@@ -58,7 +64,7 @@ func HandleNewInstall() error {
 
 	fmt.Printf("Linking %s to %s\n", me, linkTo)
 
-	if err := sudoLn(me, filepath.Join(dir, linkTo)); err != nil {
+	if err := linkFiles(me, filepath.Join(dir, linkTo)); err != nil {
 		return err
 	}
 
@@ -107,7 +113,7 @@ func setnpmPrefix() error {
 	return err
 }
 
-func sudoLn(path1, path2 string) error {
+func linkFiles(path1, path2 string) error {
 	// ????
 	f1, err := os.Stat(path1)
 	if err != nil {
@@ -118,14 +124,57 @@ func sudoLn(path1, path2 string) error {
 		return fmt.Errorf("%s is not a file", path1)
 	}
 
-	f2, err := os.Stat(path2)
+	err = isWritable(filepath.Dir(path2))
+
 	if err != nil {
-		return fmt.Errorf("unable to access file %s, err: %v", path2, err)
+		if errors.Is(err, errNotWriteable) {
+			return exec.Command("sudo", "ln", "-s", path1, path2).Run()
+		}
+
+		return fmt.Errorf("failed to link files, err: %w", err)
 	}
 
-	if !f2.IsDir() {
-		return fmt.Errorf("%s is not a file", path2)
+	return os.Symlink(path1, path2)
+}
+
+func isWritable(dir string) error {
+	// expect dir
+	s, err := os.Stat(dir)
+	if err != nil {
+		return err
 	}
 
-	return exec.Command("sudo", "ln", "-s", path1, path2).Run()
+	if !s.IsDir() {
+		return fmt.Errorf("%s is not a directory", dir)
+	}
+
+	perms := s.Mode().Perm()
+
+	if perms&0002 == 0002 { // other writeable, ownership doesn't matter
+		return nil
+	}
+
+	me, err := user.Current()
+	if err != nil {
+		return err
+	}
+
+	sysStat, ok := s.Sys().(*syscall.Stat_t)
+	if !ok {
+		return fmt.Errorf("unable to detect dir ownershit for %s", dir)
+	}
+
+	uid, _ := strconv.Atoi(me.Uid)
+
+	gid, _ := strconv.Atoi(me.Gid)
+
+	if sysStat.Uid == uint32(uid) && perms&0200 == 0200 {
+		return nil
+	}
+
+	if sysStat.Gid == uint32(gid) && perms&0020 == 0020 {
+		return nil
+	}
+
+	return errNotWriteable
 }
