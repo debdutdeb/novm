ce54fa35528f4a931f5d793013d84c3e3529af9b
Debdut Chakraborty <debdutdeb@outlook.com>
2026-03-27 19:36:54 +0530

    skip tasks if in non interactive mode

--- main.go
+++ main.go
@@ -7,9 +7,24 @@ import (
 	"github.com/debdutdeb/node-proxy/commands"
 	"github.com/debdutdeb/node-proxy/utils"
 	"github.com/debdutdeb/node-proxy/versions"
+
+	"golang.org/x/sys/unix"
 )
 
+func isInteractive() bool {
+	_, err := unix.IoctlGetTermios(int(os.Stdout), unix.TCGETS)
+	return err == nil
+}
+
 func main() {
+	if !isInteractive() {
+		if err := commands.Run(); err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+		return
+	}
+	
 	if err := utils.HandleNewInstall(); err != nil {
 		log.Fatal("failed to run fresh install tasks: ", err)
 	}
